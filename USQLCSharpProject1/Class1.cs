4a37e4d8038dfa37eae1be6efe5214fb05638ff6
Gten71 <mresnyansky@gmail.com>
2022-12-06 20:08:35 +0300

    Добавьте файлы проекта.

---
+++ USQLCSharpProject1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace USQLCSharpProject1
+{
+    public class Class1
+    {
+        public double Sec(double x)
+        {
+            x = Double.Parse(Console.ReadLine());
+            x = 1 / Math.Cos(x);
+            return x;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using USQLCSharpProject1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double x = 1;
+            double b = 1.00015233;
+            Class1 c = new Class1();
+            double actual = c.Sec(x);
+            Assert.AreEqual(b, actual, "Не правельный результат");
+
+        }
+    }
+}
